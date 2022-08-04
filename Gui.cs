42bc768278493cb5b0d5072278344a812512d4d9
Kevin Saanen <kevin@saanen.me>
2022-08-04 16:55:02 +0200

    add Lists

--- Gui.cs
+++ Gui.cs
@@ -1,4 +1,24 @@
+using System.Collections.Generic;
+
 namespace SGUI;
 public class Gui {
+  public List<GuiWidget> Widgets;
+
   public Gui() {}
+
+  public void AddWidget(GuiWidget widget) {
+    Widgets.Add(widget);
+  }
+
+  public void Update() {
+    foreach (GuiWidget widget in GuiWidget) {
+      widget.Update();
+    }
+  }
+
+  public void Draw() {
+    foreach (GuiWidget widget in GuiWidget) {
+      widget.Draw();
+    }
+  }
 }
--- GuiWidget.cs
+++ GuiWidget.cs
@@ -1,11 +1,25 @@
+using System.Collections.Generic;
+
 namespace SGUI;
 /// <summary>
 /// GuiWidget is a container for grouping controls.
 /// </summary>
 public class GuiWidget: GuiEntity {
-  public GuiControl[] Controls;
+  public List<GuiControl> Controls;
+
   public GuiWidget() {
-    
+    Controls = new List<GuiControl>();
+  }
+
+  public GuiWidget(GuiControl[] controls) {
+    Controls = new List<GuiControl>();
+    foreach(Control control in controls) {
+      Controls.Add(control);
+    }
+  }
+
+  public void AddControl(GuiControl control) {
+    Controls.Add(control);
   }
 
   public override void Draw()
@@ -14,8 +28,8 @@ public class GuiWidget: GuiEntity {
     base.Draw();
 
     // Then draw controls on top of this widget
-    foreach (GuiControl Control in Controls) {
-      Control.Draw();
+    foreach (GuiControl control in Controls) {
+      control.Draw();
     }
   }
 
